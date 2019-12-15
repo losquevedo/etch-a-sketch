afa6dab09f3903664de7a7fe8ef4c1b5437f5131
Carlos Quevedo <carlosqueis@gmail.com>
2019-12-15 13:31:48 -0800

    Adding project requirements and basic html

---
+++ etch-a-sketch-app.js
@@ -0,0 +1,10 @@
+// Create a webpage with a 16x16 grid of square divs 
+
+// Set up a “hover” effect so that the grid divs change color 
+// when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. 
+
+//Add a button to the top of the screen which will clear the current grid and send the user a popup asking for how many squares per side to make the new grid. 
+//Once entered the new grid should be generated in the same total space as before (e.g. 960px wide) and now you’ve got a new sketch pad. 
+
+// (Optional): Instead of just changing the color of your grid from black to white (for example) have each pass through it with the mouse change to a completely random RGB value. 
+// Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
