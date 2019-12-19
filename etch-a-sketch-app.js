ae7df7e6d8dcebb7eff41c0d3e5d7288c6f5bd19
Carlos Quevedo <carlosqueis@gmail.com>
2019-12-18 22:18:38 -0800

    Adding initial code to hopefully create boxes

--- etch-a-sketch-app.js
+++ etch-a-sketch-app.js
@@ -1,4 +1,11 @@
 // Create a webpage with a 16x16 grid of square divs 
+const container = document.querySelector("#container");
+        
+for(i=0; i < 17; i++) {
+    box = document.createElement("div");
+    box.classList.add("box " + i);
+    container.appendChild(box);
+}
 
 // Set up a “hover” effect so that the grid divs change color 
 // when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. 
