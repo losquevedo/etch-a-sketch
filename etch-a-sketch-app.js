391acf19139211ced99f7172251a095de8fbd5db
Carlos Quevedo <cquevedo@lyft.com>
2019-12-30 15:56:37 -0800

    Added beginning of hover event for cells but it's still not working 100%

--- etch-a-sketch-app.js
+++ etch-a-sketch-app.js
@@ -54,6 +54,17 @@ window.onload=function() {
 
 // Set up a “hover” effect so that the grid divs change color 
 // when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. 
+container.forEach(cell => {
+    cell.addEventListener('mouseover', (e) => {
+        e.target.style.color = 'black';
+
+        //set a timeout for the hover over
+        setTimeout(function() {
+            e.target.style.color = "";
+        }, 500);
+    });
+
+});
 
 //Add a button to the top of the screen which will clear the current grid and send the user a popup asking for how many squares per side to make the new grid. 
 //Once entered the new grid should be generated in the same total space as before (e.g. 960px wide) and now you’ve got a new sketch pad. 
